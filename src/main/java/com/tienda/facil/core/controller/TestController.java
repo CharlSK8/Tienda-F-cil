88481dde7d5ea2c356a31bb7acc7be3185d94b25
Charlinson Perez <hperez@qvision.com.co>
2024-11-14 20:05:53 -0500

    docs: elimina documentación de parámetro no necesario

--- src/main/java/com/tienda/facil/core/controller/TestController.java
+++ src/main/java/com/tienda/facil/core/controller/TestController.java
@@ -1,14 +1,13 @@
 package com.tienda.facil.core.controller;
 
-import com.tienda.facil.core.dto.ResponseDTO;
-import com.tienda.facil.core.dto.TestDTO;
+import com.tienda.facil.core.dto.response.ResponseDTO;
+import com.tienda.facil.core.dto.request.TestDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,22 +22,14 @@ public class TestController {
      *
      * @param testDTO        El objeto de transferencia de datos {@link TestDTO} que contiene los datos de prueba
      *                       enviados en el cuerpo de la solicitud.
-     * @param bindingResult  El resultado de la validación del objeto {@link TestDTO}.
-     *                       Permite verificar si existen errores en la validación.
-     *
      * @return {@link ResponseEntity} con un {@link ResponseDTO} que contiene el mensaje de bienvenida si el
      *         {@link TestDTO} es válido, o un mensaje de error si hay errores de validación.
      *
      */
     @Operation(summary = "Test Controller", description = "Devuelve un mensaje de bienvenida con el nombre proporcionado")
     @PostMapping("/test")
-    public ResponseEntity<ResponseDTO> testController(@Valid @RequestBody TestDTO testDTO, BindingResult bindingResult) {
+    public ResponseEntity<ResponseDTO<Object>> testController(@Valid @RequestBody TestDTO testDTO) {
         try {
-            if (bindingResult.hasErrors()) {
-                String errorMessage = bindingResult.getFieldError().getDefaultMessage();
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResponseDTO.builder().response(errorMessage)
-                        .code(HttpStatus.BAD_REQUEST.value()).build());
-            }
             String responseMessage = "Welcome to tienda fácil, " + testDTO.getName();
             return ResponseEntity.status(HttpStatus.OK).body(ResponseDTO.builder().response(responseMessage)
                     .code(HttpStatus.OK.value()).message("Se procesó correctamente la solicitud").build()) ;
